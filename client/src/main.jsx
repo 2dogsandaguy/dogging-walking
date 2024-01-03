04b7fb10652fb7197521fa95ac0981f755852681
2dogsandaguy <Neverclear@ymail.com>
2024-01-02 22:01:06 -0500

    get home.jsx to show up in url

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+ import Home from "../pages/home.jsx"; 
+
+const route = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    /* errorElement: <Error />, */
+    children: [
+       {
+        index: true,
+        element: <Home/>,
+      }, 
+      /* {
+        path: 'SignUp',
+        element: <SignUp />,
+      },
+      {
+        path: 'Profile',
+        element: <Profile />
+      },
+      {
+        path: 'CreateWorkout',
+        element: <CreateWorkout />
+      },
+      {
+        path: 'ViewHistory',
+        element: <ViewHistory />
+      }, */
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={route} />
+);
